8742488fd75151f6b823008a448b6a206951e93c
A. Spivack <spivack@gmail.com>
2017-03-13 22:36:31 -0700

    Missed a few files and refactoring Slots.Slot.h

--- ScheduleEditor/Slots.Slot.h
+++ ScheduleEditor/Slots.Slot.h
@@ -1,11 +1,26 @@
 #pragma once
 
 namespace Model::Slots {
-	struct Slot {
+
+	using namespace Model;
+
+	class Slot 
+	{
 	Properties::PropertyMap mProperty;
 	Model::Time::HourMinute start;
 	Model::Time::Duration duration;
 	//const SlotGroup * const mOwner;
+	public:
+		const Properties::PropertyMap & properties_get();
+
+		template<class DurationType>
+		void duration_set(DurationType t) {
+			duration = t;
+		}
+
+		Time::HourMinute start_get() const;
+
+		void start_set(Time::HourMinute tm);
 
 	private:
 	uint32_t	mSlotOrder;
--- ScheduleEditor/Slots.h
+++ ScheduleEditor/Slots.h
@@ -1,8 +1,15 @@
 #pragma once
 
-
+#include "TimeDuration.h"
 
 namespace Model::Slots {
-	
-	
-	}
+	/// A item that represents a time-period or organizing block
+	/// Slots are strongly ordered represented by mSlotOrder
+
+
+	const SlotGroup & FindSlotGroup(std::string name);
+	const SlotGroup & FindSlotGroup(Model::Index modelIndex);
+
+	SlotGroup & CreateSlotGroup(std::string name);
+	void SaveSlotGroup(SlotGroup & group);
+}
