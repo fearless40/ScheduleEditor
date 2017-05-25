da6f7b0c239618554ea37baf7ae80ec18a6880a9
Adam Spivack <spivack@outlook.com>
2017-05-25 15:48:56 -0700

    Continue to fix my though experiments. Will most likely require adjustments as the code is actually used.

--- ScheduleEditor/Model/Resources.Resource.h
+++ ScheduleEditor/Model/Resources.Resource.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "Resources.h"
+#include "Properties.PropertyMap.h"
 
 namespace Model::Resources {
 	
 	/// Holds the data for each event/person/place. Flexible and owned by ResourceType
-	class Resource
+	class Resource : public Properties::PropertyMap
 	{
-		Properties::PropertyMap mProperties;
 		const ResourceType & mType;
+		bool mIsDeleted;
 		
 		// Only writable by ResourceType
 		ResourceUniqueID mUnique;
@@ -19,9 +20,10 @@ namespace Model::Resources {
 		/// Checks to see if the resouce has been deleted. Deleted resource are still kept around unless all references to them have been removed
 		/// <remarks> Resource are not deleted right away as parts of the schedule will still rely on them. The data will still
 		/// exist in the file however it will no longer be editable. The resource will not be selected for new data entry. </remarks>
-		bool deleted() const;
+		bool isDeleted() const { return mIsDeleted; }
 
-		const Model::Properties::PropertyMap & properties_get();
+		const ResourceType & getType() const { return mType; }
+
 
 		// Helpful to allow if() like constructs to check if something is empty or null
 		operator bool() { return (this != &NullResource); }
--- ScheduleEditor/Model/Resources.ResourceType.h
+++ ScheduleEditor/Model/Resources.ResourceType.h
@@ -10,11 +10,6 @@ namespace Model::Resources  {
 	/// Owned by: self, obtain by using static functions
 	class ResourceType : public ModelIndex<ResourceType>
 	{
-		//Index mIndex;
-		//std::string mName;
-		//std::vector<Resource> mMembers;
-		//const PropertyTemplate * mTemplate;
-
 	public:
 
 		Model::Index index() const { return mIndex;  }
@@ -34,6 +29,8 @@ namespace Model::Resources  {
 		const Model::Properties::PropertyTemplate & propertyTemplate() const { return mPropTemp; }
 		Model::Properties::PropertyTemplate & propertyTemplate() { return mPropTemp;  }
 
+		const auto & properties() const { return mProperties; }
+		auto & properties() { return mProperties; }
 
 		// Static functions Inherited from the template
 		/*static const ResourceType & Find(Model::Index name);
@@ -43,7 +40,13 @@ namespace Model::Resources  {
 		std::vector<Model::Index> GetAllNames();*/
 	private:
 		Model::Index mIndex;
+		
+		// Todo: determine if having a vector here is better or a list. A list allows direct memory access without having to worry about a resource being moved.
+		// The vector is much faster iteration at the cost of greater difficluty coding as it is safe for reading at the same time but not editing. It would therefore
+		// require mutexs and such to get it to work.
 		Utility::FixedList<Resource> mItems;
+		
 		Model::Properties::PropertyTemplate mPropTemp;
+		Model::Properties::PropertyMap mProperties;
 	};
 }
