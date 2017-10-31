03041b799d2e8a24b5592fd424ae2cc84836232f
A. Spivack <spivack@gmail.com>
2017-10-30 22:45:44 -0700

    Just wrapping std::unordered to make life a little easier in ModelIndex.

--- Model/ModelCollection.h
+++ Model/ModelCollection.h
@@ -3,6 +3,7 @@
 
 #include "Model.h"
 #include <utility>
+#include <unordered_map>
 //#include <boost\container\flat_map.hpp>
 //using namespace boost::multi_index;
 
@@ -10,61 +11,128 @@ namespace Model {
 
 	template<typename T>
 	Model::IndexConst getIndex(const T & t) {
-		return t->index();
+		return t.index();
 	}
 
+	template<typename T>
+	Model::IndexConst getIndex(const std::unique_ptr<T> & t) {
+		return t->index();
+	}
+	
+	
 	Model::IndexConst getIndex(Model::IndexConst idx) {
 		return idx;
 	}
 
-	template <class T>
+	struct ModelCollection_TagVector {};
+	struct ModelCollection_TagUnordered {};
+
+	template <class T, class tag = ModelCollection_TagUnordered >
 	class ModelCollection {
+		using Set = std::unordered_map<Model::Index, T>;
+
+		Set mItems;
+
+	public:
+		const T * find(Model::IndexConst idx) const {
+			auto item = mItems.find(Model::Index{ idx });
+			if (item != mItems.end()) {
+				return &item->second;
+			}
+			return nullptr;
+		}
+
+		bool save(T && value) {
+			[[maybe_unused]] auto[where, inserted] = mItems.insert(
+			std::pair<Model::IndexConst, T &&>{ Model::getIndex(value), std::move(value) });
+			return inserted;
+		}
+
+		bool save(const T & value) {
+			[[maybe_unused]]auto[where, inserted] = mItems.insert(
+			std::pair<Model::IndexConst, const T &>{ Model::getIndex(value), value });
+			return inserted;
+		}
+
+		void erase(Model::IndexConst idx) {
+			mItems.erase(Model::Index{ idx });
+		}
+
+		void save(std::vector<T> && values) {
+			mItems.reserve(values.size() + mItems.size());
+			for (auto && x : values) {
+				mItems.insert(
+					std::pair<Model::IndexConst, T &&>{ Model::getIndex(x), std::move(x) });
+			}
+		}
+
+		const std::vector<Model::IndexConst> enumerate_indexs() const {
+			std::vector<Model::IndexConst> values{ mItems.size() };
+			std::transform(mItems.begin(), mItems.end(), std::back_inserter(values),
+				[](auto & x) { return getIndex(x.second); });
+			return values;
+		}
+	};
+
+	template <class T>
+	class ModelCollection<T, ModelCollection_TagVector>{
 
 		//using Set = boost::container::flat_map<Model::Index,std::unique_ptr<T>>;
 		using Set = std::vector<std::unique_ptr<T>>;
 
 		Set mItems;
 
-		
+		void _push_back(T && item) {
+			mItems.push_back(std::move(std::make_unique<T>(item)));
+		}
 
-		auto _find(Model::IndexConst idx) {
+		auto _find(Model::IndexConst idx) const {
 			return std::lower_bound(mItems.begin(), mItems.end(), idx,
 				[](auto && a, auto && b) {
 				return getIndex(a) < getIndex(b);
 			});
 		}
 
+		void _sort() {
+			std::sort(mItems.begin(), mItems.end());
+		}
+
 
 	public:
-		const T * find(Model::IndexConst idx) {
-			if (auto it = _find(idx); it != mItems.end() && getIndex(*it) == idx) {
+		const T * find(Model::IndexConst idx) const {
+			
+			if (const auto it = _find(idx); it != mItems.end() && getIndex(*it) == idx) {
 				return it->get();
 			}
 			return nullptr;
 		}
 
 		bool save(T && value) {
-			mItems.push_back(std::make_unique<T>(std::move(value)));
+			_push_back(std::move(value));
+			_sort();
 			return true;
 		}
 
 		bool save(const T & value) {
 			mItems.push_back(std::make_unique<T>(value));
+			_sort();
 			return true;
 		}
 
 		void erase(Model::IndexConst idx) {
-			mItems.erase(idx);
+			mItems.erase(_find(idx));
+			_sort();
 		}
 
-		/*bool save(std::vector<T> && values) {
+		void save(std::vector<T> && values) {
 			mItems.reserve(values.size() + mItems.size());
 			for (auto & x : values) {
-				save(std::move(x));
+				_push_back(std::move(x));
 			}
-		}*/
+			_sort();
+		}
 
-		const std::vector<Model::IndexConst> enumerate_indexs() {
+		const std::vector<Model::IndexConst> enumerate_indexs() const {
 			std::vector<Model::IndexConst> values{ mItems.size() };
 			std::transform(mItems.begin(), mItems.end(), std::back_inserter{ values },
 				[](auto & x) { return x->get()->index(); });
--- ModelTest/ModelCollectionTest.cpp
+++ ModelTest/ModelCollectionTest.cpp
@@ -4,17 +4,31 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+
 namespace Model_ModelCollection {
+
+	struct Test {
+		Model::Index mIndex;
+		Model::IndexConst index() const noexcept { return mIndex; }
+		Test(int index) {
+			mIndex = std::to_string(index);
+		}
+
+		Test(Model::IndexConst idx) : mIndex(idx) {};
+	};
+
+	template <typename T>
+	void insert_values(int start, int end, T & t) {
+		for (int i = start; i < end; ++i) {
+			t.save(Test{ i });
+		}
+	}
 
 
 	TEST_CLASS(PropertiesMap)
 	{
 	public:
-		TEST_METHOD(Create) {
-			struct Test {
-				Model::Index mIndex;
-				Model::IndexConst index() const noexcept { return mIndex; }
-			};
+		TEST_METHOD(Create_Unordered) {
 			
 			Model::ModelCollection<Test> values;
 			
@@ -42,6 +56,50 @@ namespace Model_ModelCollection {
 				Test t{ "5" };
 				values.save(std::move(t));
 			}
+
+			Assert::AreEqual(values.find("4")->mIndex, std::string { "4" });
+			Assert::IsNull(values.find("10"));
+		}
+
+		TEST_METHOD(Erase) {
+			Model::ModelCollection<Test> values;
+			insert_values(1, 10, values);
+			values.erase("5");
+			Assert::IsNull(values.find("5"));
+		}
+
+		TEST_METHOD(Insert_Many) {
+			Model::ModelCollection<Test> values;
+			std::vector<Test> add_many;
+			for (int i = 0; i < 20; ++i) {
+				add_many.emplace_back(i);
+			}
+			values.save(std::move(add_many));
+
+			Assert::IsNotNull(values.find("19"));
+		}
+
+		TEST_METHOD(Enumerte) {
+			Model::ModelCollection<Test> values;
+			insert_values(1, 10, values);
+			const auto k = values.enumerate_indexs();
+			bool ok = false;
+			for (int i = 1; i < 10; ++i) {
+				ok = std::find(k.begin(), k.end(), std::to_string(i)) != k.end();
+				if (!ok) {
+					Assert::Fail(L"Not all expected keys found.");
+				}
+			}
+			Assert::IsTrue(true);
+			
+		}
+		
+
+		TEST_METHOD(Create_Vector) {
+
+			Model::ModelCollection<Test, Model::ModelCollection_TagVector > values;
+
+			insert_values(1, 5, values);
 
 			Assert::AreEqual(values.find("4")->mIndex, std::string { "4" });
 			Assert::IsNull(values.find("10"));
